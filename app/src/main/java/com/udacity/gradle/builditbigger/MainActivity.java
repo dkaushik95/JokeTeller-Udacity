5159eac01b6530e77cb8af0d9059c6c60ed1c42d
Dishant Kaushik <dkaushik95@gmail.com>
2016-09-08 21:39:46 +0530

    Added an android Library to show the joke passed by an intent: The Joke works like this, the client requests for the joke-> Jibrary executes-> Sends joke to the GCE-> Sends joke to the Main Activity-> Sends the joke as an intent ! And it works :D

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -41,7 +42,9 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "DERP2", Toast.LENGTH_SHORT).show();
+        //TODO: Connect the ASYNC task and execute on onclick
+        new EndpointsAsyncTask().execute(this);
+        //Toast.makeText(this, "DERP2", Toast.LENGTH_SHORT).show();
     }
 
 
